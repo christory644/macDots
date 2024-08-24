b0f667125dacd34830049e9ddfafadd2d1e3c233
Christopher Story <christory644@pm.me>
2024-08-23 19:09:03 -0500

    update treesitter config for nvim

--- config/nvim/lua/christory/plugins/treesitter.lua
+++ config/nvim/lua/christory/plugins/treesitter.lua
@@ -10,90 +10,91 @@ return {
     local treesitter = require("nvim-treesitter.configs")
 
     -- configure treesitter
-    treesitter.setup(
-      {
-        -- automatically install missing parsers when entering a buffer
-        -- not recommended if you don't have the tree sitter cli installed
-        -- use npm -g i tree-sitter-cli to install it
-        auto_install = true,
-        -- enable autotagging (with nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
+    treesitter.setup({
+      -- automatically install missing parsers when entering a buffer
+      -- not recommended if you don't have the tree sitter cli installed
+      -- use npm -g i tree-sitter-cli to install it
+      auto_install = true,
+      -- enable autotagging (with nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
+      -- ensure the following language parsers are always installed
+      ensure_installed = {
+        "bash",
+        "c",
+        "clojure",
+        "comment",
+        "commonlisp",
+        "cpp",
+        "css",
+        "csv",
+        "cuda",
+        "elixir",
+        "elm",
+        "erlang",
+        "gdscript",
+        "git_config",
+        "gitignore",
+        "gleam",
+        "go",
+        "graphql",
+        "haskell",
+        "helm",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "jq",
+        "json",
+        "kotlin",
+        "lua",
+        "make",
+        "markdown",
+        "nix",
+        "ocaml",
+        "perl",
+        "php",
+        "prisma",
+        "python",
+        "ruby",
+        "rust",
+        "scala",
+        "scss",
+        "solidity",
+        "sql",
+        "svelte",
+        "terraform",
+        "tmux",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "vue",
+        "xml",
+        "yaml",
+        "zig",
+      },
+      -- enable syntax highlighting
+      highlight = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
         },
-        -- ensure the following language parsers are always installed
-        ensure_installed = {
-          "bash",
-          "c",
-          "clojure",
-          "comment",
-          "commonlisp",
-          "cpp",
-          "css",
-          "csv",
-          "cuda",
-          "elixir",
-          "elm",
-          "erlang",
-          "gdscript",
-          "git_config",
-          "gitignore",
-          "gleam",
-          "go",
-          "graphql",
-          "haskell",
-          "helm",
-          "html",
-          "http",
-          "java",
-          "javascript",
-          "jq",
-          "json",
-          "kotlin",
-          "lua",
-          "make",
-          "nix",
-          "ocaml",
-          "perl",
-          "php",
-          "prisma",
-          "python",
-          "ruby",
-          "rust",
-          "scala",
-          "scss",
-          "solidity",
-          "sql",
-          "svelte",
-          "terraform",
-          "tmux",
-          "toml",
-          "tsx",
-          "typescript",
-          "vue",
-          "xml",
-          "yaml",
-          "zig",
-        },
-        -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-        -- enable indentation
-        indent = {
-          enable = true,
-        },
-        -- install parsers asynchronously (only applies to `ensure_installed`)
-        sync_install = false,
-      }
-    )
+      },
+      -- enable indentation
+      indent = {
+        enable = true,
+      },
+      -- install parsers asynchronously (only applies to `ensure_installed`)
+      sync_install = false,
+    })
   end,
 }
