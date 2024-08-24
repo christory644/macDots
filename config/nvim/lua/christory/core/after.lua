5375dfbaa6ae950aa4da7650e68d4be692ad30b2
Christopher Story <christory644@pm.me>
2024-08-23 19:10:22 -0500

    add keybinds for substitue plugin

--- config/nvim/lua/christory/core/after.lua
+++ config/nvim/lua/christory/core/after.lua
@@ -4,7 +4,7 @@
 vim.cmd.colorscheme("night-owl")
 
 -- substitution
-vim.keymap.set("n", "s", require('substitute').operator, { noremap = true, desc = "Substitue with motion" })
-vim.keymap.set("n", "ss", require('substitute').line, { noremap = true, desc = "Substitue line" })
-vim.keymap.set("n", "S", require('substitute').eol, { noremap = true, desc = "Substitue to end of line" })
-vim.keymap.set("x", "s", require('substitute').visual, { noremap = true, desc = "Substitue in visual mode" })
+vim.keymap.set("n", "s", require("substitute").operator, { noremap = true, desc = "Substitue with motion" })
+vim.keymap.set("n", "ss", require("substitute").line, { noremap = true, desc = "Substitue line" })
+vim.keymap.set("n", "S", require("substitute").eol, { noremap = true, desc = "Substitue to end of line" })
+vim.keymap.set("x", "s", require("substitute").visual, { noremap = true, desc = "Substitue in visual mode" })
